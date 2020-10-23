a6363b5bdb754ee702d90f2ab6bec8d6e9453701
soyalatte <uyyu1734@gmail.com>
2020-10-23 10:30:02 +0900

    innerHTML로 리스트 추가

--- src/main.js
+++ src/main.js
@@ -3,57 +3,64 @@
 //item 이라는 div를 만들어 이미지와 속성을 넣는다
 // item들을 item-list에 추가한다
 let itemList = [];
+
 function loadItems() {
   return fetch("./data/data.json")
     .then((response) => response.json())
     .then((json) => json.items);
 }
 
+// 클릭시 이벤트 발생 : item 제거 후 해당 속성인 아이템만 추가
 function showItems(items) {
   const list = document.querySelector(".item-list");
   let last;
   while ((last = list.lastChild)) list.removeChild(last);
-  items.forEach((item) => {
-    const listItem = document.createElement("li");
-    listItem.setAttribute("class", "item");
-    const attribute = document.createElement("div");
-    //item의 텍스트
-    let str = "";
-    for (let value in item) {
-      if (value === "img") continue;
-      str += `${item[value]}, `;
-    }
-    str = str.slice(0, str.length - 2);
-    const text = document.createTextNode(str);
 
-    //item의 이미지
-    const img = document.createElement("img");
-    img.src = item["img"];
-    img.width = 50;
-    listItem.appendChild(img);
-    attribute.appendChild(text);
-    listItem.appendChild(attribute);
+  // array.map : 한 배열의 요소들을 다른 요소로 변경 한다.
+  // array.join : 문자열 배열을 하나의 문자열로 생성 .
+  // map 으로 배열안 객체를 <li>...</li> 형태로 바꾼뒤 join 을 통해 한 문자열로 만들어 추가
+  list.innerHTML = items.map((item) => createHTMLString(item)).join("");
 
-    //console.log(item);
-    list.appendChild(listItem);
-  });
+  // items.forEach((item) => {
+  //   const listItem = document.createElement("li");
+  //   listItem.setAttribute("class", "item");
+  //   const attribute = document.createElement("div");
+  //   //item의 텍스트
+  //   let str = "";
+  //   for (let value in item) {
+  //     if (value === "img") continue;
+  //     str += `${item[value]}, `;
+  //   }
+  //   str = str.slice(0, str.length - 2);
+  //   const text = document.createTextNode(str);
 
-  //일단 있는걸 비워준다
-  //들어온 아이템으로 새로 추가해준다
+  //   //item의 이미지
+  //   const img = document.createElement("img");
+  //   img.src = item["img"];
+  //   img.width = 50;
+  //   listItem.appendChild(img);
+  //   attribute.appendChild(text);
+  //   listItem.appendChild(attribute);
+
+  //   //console.log(item);
+  //   list.appendChild(listItem);
+  // });
 }
 
-function clickBtn(event) {
-  console.log(event.target);
+function createHTMLString(item) {
+  return `
+  <li class="item">
+    <img src="${item.img}" alt="${item.type}" class="item__image"/>
+    <span class="item__description">${item.sex}, ${item.size}</span>
+  </li>`;
 }
 
 function setEventListeners() {
   let category = document
     .querySelector(".category")
     .addEventListener("click", (event) => {
-      const target = event.target;
-      const filter = target.id;
+      const filter = event.target.id;
       if (!filter) return;
-      console.log(filter);
       filterItems(filter);
     });
 }
@@ -68,6 +75,7 @@ function filterItems(value) {
   showItems(filteredItems);
 }
 
+//main
 loadItems()
   .then((items) => {
     itemList = items;
@@ -75,6 +83,3 @@ loadItems()
     setEventListeners(items); //상단에 하나 달아주면될듯
   })
   .catch(console.log);
-
-//filter 에 마우스가 올라가면 사이즈 커지게
-// 클릭시 이벤트 발생 : item 제거 후 해당 속성인 아이템만 추가
